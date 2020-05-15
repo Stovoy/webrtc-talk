9e2465786f10a175770cdb50fb619f9b39744965
Steve Mostovoy <steve.mostovoy@coinbase.com>
2020-05-15 12:04:03 -0700

    Updates

---
+++ precache-manifest.9729deb6c5bd9196ef8a5309ba973fb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5b35b806c1746adc875c3e904ad55c4",
+    "url": "/webrtc-talk/static/media/webrtcClient.f5b35b80.png"
+  },
+  {
+    "revision": "22ce31b1c6f9994011741644d6242b32",
+    "url": "/webrtc-talk/static/media/server.22ce31b1.png"
+  },
+  {
+    "revision": "510e8244a98d99ca4af851931a7d2c3a",
+    "url": "/webrtc-talk/static/media/sendState.510e8244.png"
+  },
+  {
+    "revision": "fccddc688c6a0a3564e9a04abbed3ed3",
+    "url": "/webrtc-talk/static/media/gameLoop.fccddc68.png"
+  },
+  {
+    "revision": "6e85baa725b1fe708c2bd1d3538655d1",
+    "url": "/webrtc-talk/static/media/clientServer.6e85baa7.png"
+  },
+  {
+    "revision": "cde35aea086f978423ed",
+    "url": "/webrtc-talk/static/js/runtime~main.cde35aea.js"
+  },
+  {
+    "revision": "285c0f59e8f5174cae9b",
+    "url": "/webrtc-talk/static/js/main.285c0f59.chunk.js"
+  },
+  {
+    "revision": "b9713776c2e4910fcae4",
+    "url": "/webrtc-talk/static/js/1.b9713776.chunk.js"
+  },
+  {
+    "revision": "b9713776c2e4910fcae4",
+    "url": "/webrtc-talk/static/css/1.d1a4e84b.chunk.css"
+  },
+  {
+    "revision": "8e1b4c4f2581a3777bd08aee2cb0ba11",
+    "url": "/webrtc-talk/index.html"
+  }
+];
